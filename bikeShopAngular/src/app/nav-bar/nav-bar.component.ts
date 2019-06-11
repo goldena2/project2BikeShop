177391989ea67449ba2218e00c09756c431b04b2
goldena2 <goldena2@wwu.edu>
2019-06-11 09:50:12 -0400

    some stuff

--- bikeShopAngular/src/app/nav-bar/nav-bar.component.ts
+++ bikeShopAngular/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  @Input() otherLinks: object[];
 
-  constructor() { }
+
+  constructor() { 
+    console.log(this.otherLinks);
+  }
 
   ngOnInit() {
   }
