79d016e24deb9257e93683310383efc5eeae92e5
Mitch6706 <MitchAndrewMiller@gmail.com>
2019-06-12 12:29:39 -0400

    commit

--- bikeShopAngular/src/app/make-customer-acount/make-customer-acount.component.ts
+++ bikeShopAngular/src/app/make-customer-acount/make-customer-acount.component.ts
@@ -7,16 +7,16 @@ import { MakeAccountService } from '../make-account.service';
   styleUrls: ['./make-customer-acount.component.css']
 })
 export class MakeCustomerAcountComponent{
-  constructor(private accountBuilder : MakeAccountService) { 
+  constructor(private accountBuilder: MakeAccountService) {
   }
 
 
-  makeAcount(username: string, password: string, number: string, fname: string, lname: string, email:string){
+  makeAcount(username: string, password: string, number: string, fname: string, lname: string, email:string) {
     this.accountBuilder.create(username, password, number, fname, lname, email, 1).subscribe(data => {
         if(data['success']){
           console.log('wat')
-        }else{
-          alert("Unable to make an account at this time please try again later");
+        } else {
+          alert('Unable to make an account at this time please try again later');
         }
       });
   }
