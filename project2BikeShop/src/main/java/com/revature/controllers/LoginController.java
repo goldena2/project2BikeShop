d6675d903b7d03c1dbb6e13c40ab0e8a45d8322f
Mitch6706 <MitchAndrewMiller@gmail.com>
2019-06-12 21:48:48 -0400

    commit

--- project2BikeShop/src/main/java/com/revature/controllers/LoginController.java
+++ project2BikeShop/src/main/java/com/revature/controllers/LoginController.java
@@ -5,17 +5,19 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.beans.User;
 import com.revature.data.UserDAO;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value="/login")
 public class LoginController {
-	@Autowired
+	//@Autowired
 	private UserDAO userDAO;
 	
 	@RequestMapping(method=RequestMethod.GET)
@@ -27,10 +29,18 @@ public class LoginController {
 		return "static/login.html";
 	}
 	
-
 	@PostMapping
-	public Boolean login(String username, String password, HttpSession session) {
+	public Boolean login(@RequestBody User login, HttpSession session) {//String username, String password, HttpSession session) {
 		System.out.println("Getting log in post request");
-		return true;
+		System.out.println(login);
+		User user = userDAO.getUser(login.getUsername(), login.getPassword());
+		System.out.println(user);
+		if(user!=null) {
+			System.out.println("sending user value");
+			session.setAttribute("user", user);
+			return true;
+		}
+		System.out.println("No user found.");
+		return false;
 	}
 }
