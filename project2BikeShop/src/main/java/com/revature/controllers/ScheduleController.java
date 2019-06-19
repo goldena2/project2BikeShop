6c84a872d563d7dbdebdfc90a7434e84cc18f0af
Patrick Call <patrickscall5@gmail.com>
2019-06-19 12:31:01 -0400

    commiting

--- project2BikeShop/src/main/java/com/revature/controllers/ScheduleController.java
+++ project2BikeShop/src/main/java/com/revature/controllers/ScheduleController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.beans.Product;
+import com.revature.beans.Day;
+import com.revature.beans.Schedule;
+import com.revature.beans.Shift;
+import com.revature.data.ScheduleDOA;
 import com.revature.services.GenerateSchedule;
 
 @CrossOrigin
@@ -20,10 +22,35 @@ import com.revature.services.GenerateSchedule;
 public class ScheduleController {
 	@Autowired
 	GenerateSchedule gs;
+	@Autowired
+	ScheduleDOA sd;
 	
 	@PostMapping(value="/generateSchedule")
 	public void generate(@RequestBody String[] dates){
 		System.out.println(Arrays.toString(dates));
 		gs.generate(dates[0], dates[1]);
+	}
+	
+	@GetMapping(value="/schedules")
+	public List<Schedule> getSchedules(){
+		List<Schedule> scheduleList = new ArrayList<Schedule>();
+		scheduleList = sd.getSchedules();
+		return scheduleList;
+	}
+	
+	@PostMapping(value="/personalSchedule")
+	public List<Shift> getShifts(@RequestBody Shift s){
+		List<Shift> shiftList = new ArrayList<Shift>();
+		System.out.println(s);
+		System.out.println(s.getScheduleId());
+		shiftList = sd.getShifts(s.getScheduleId());
+		System.out.println(shiftList);
+		return shiftList;
+	}
+	
+	@PostMapping(value="/getDay")
+	public Day getDay(@RequestBody Day d) {
+		Day day = sd.getDay(d.getId());
+		return day;
 	}
 }
